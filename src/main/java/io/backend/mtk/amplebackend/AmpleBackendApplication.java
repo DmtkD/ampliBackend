32a52b815996e2c0f058e3884dcfe6e7d5d04489
DmtkD <pavlo.to447@gmail.com>
2023-03-26 11:07:13 +0300

    init commit

---
+++ src/main/java/io/backend/mtk/amplebackend/AmpleBackendApplication.java
@@ -0,0 +1,13 @@
+package io.backend.mtk.amplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/backend/mtk/amplebackend/AmpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.backend.mtk.amplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
